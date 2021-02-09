3cbe1c40f0cf0f1500a9bb75cc406c3bce618d51
Noah Sadaka <noahsadaka@gmail.com>
2021-02-09 14:13:02 -0500

    linting attitudedynamics

--- attitudedynamics.py
+++ attitudedynamics.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math
-pi = math.pi
 from math import sqrt as sqrt
 from math import cos as cos
 from math import sin as sin
@@ -8,6 +7,8 @@ from math import acos as acos
 from math import asin as asin
 from math import tan as tan
 from math import atan as atan
+pi = math.pi
+
 
 def DCM_from_quaternion(e):
     """
@@ -30,7 +31,7 @@ def DCM_from_quaternion(e):
     C32 = 2*(e[1]*e[2] + e[0]*e[3])
     C33 = 1 - 2*e[0]**2 - 2*e[1]**2
 
-    DCM = np.array([[C11,C12,C13],[C21,C22,C23],[C31,C32,C33]])
+    DCM = np.array([[C11, C12, C13], [C21, C22, C23], [C31, C32, C33]])
     check_DCM(DCM)
     return DCM
 
@@ -46,17 +47,17 @@ def quaternion_from_DCM(DCM):
     """
 
     check_DCM(DCM)
-    E_4 = 0.5*math.sqrt(1 + DCM[0,0] + DCM[1,1] + DCM[2,2])
-    E_1 = (C[2,1]-C[1,2])/(4*E_4)
-    E_2 = (C[0,2]-C[2,0])/(4*E_4)
-    E_3 = (C[1,0]-C[0,1])/(4*E_4)
+    E_4 = 0.5*math.sqrt(1 + DCM[0, 0] + DCM[1, 1] + DCM[2, 2])
+    E_1 = (DCM[2, 1]-DCM[1, 2])/(4*E_4)
+    E_2 = (DCM[0, 2]-DCM[2, 0])/(4*E_4)
+    E_3 = (DCM[1, 0]-DCM[0, 1])/(4*E_4)
 
     q = np.array([E_1, E_2, E_3, E_4])
     check_quaternion(q)
     return q
 
 
-def quaternion_from_lambda_theta(lam,theta):
+def quaternion_from_lambda_theta(lam, theta):
     """
     Converts Euler axis/Euler Angle to Euler parameters
 
@@ -72,9 +73,10 @@ def quaternion_from_lambda_theta(lam,theta):
     check_quaternion(q)
     return q
 
+
 def lambda_theta_from_quaternion(e):
     """
-    Converts Euler parameters to the Euler axis/angle 
+    Converts Euler parameters to the Euler axis/angle
 
     Inputs
         e: 4-vector
@@ -89,6 +91,7 @@ def lambda_theta_from_quaternion(e):
     check_lambda(lam)
     return lam, theta
 
+
 def check_lambda(lam):
     """
     Checks that lambda is a unit vector
@@ -100,6 +103,7 @@ def check_lambda(lam):
     if tol > 1e-6:
         print(f'Lambda vector is not a unit vector {lam}')
 
+
 def check_DCM(DCM):
     """
     Checks that the DCM satisfies the orthogonality conditions
@@ -108,11 +112,12 @@ def check_DCM(DCM):
     """
     badflag = 0
     for i in range(3):
-        tol = 1-abs(sqrt(DCM[i,0]**2 + DCM[i,1]**2 + DCM[i,2]**2))
+        tol = 1-abs(sqrt(DCM[i, 0]**2 + DCM[i, 1]**2 + DCM[i, 2]**2))
         if tol > 1e-6:
             badflag = 1
     if badflag == 1:
         print(f'DCM does not satisfy orthogonality conditions {DCM}')
+
 
 def check_quaternion(e):
     """
@@ -124,6 +129,3 @@ def check_quaternion(e):
     tol = 1 - abs(summation)
     if tol > 1e-6:
         print(f'Quaternion constraint not met for e = {e}')
-
-
-
